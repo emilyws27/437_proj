1df225700a0f2326fa071bda5fb0b9c17d6ebe1a
MustafaEmreErengul <m.erengul.52@gmail.com>
2022-02-23 11:48:31 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'package:zesty/bottom_nav_bar.dart';
+
+// import 'package:zesty/bottom_nav_bar.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
