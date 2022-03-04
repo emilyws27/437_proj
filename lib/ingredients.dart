4d617a35ac05356bb06d8648c38c49a73a785de6
adamkw66 <a.knox-warshaw@wustl.edu>
2022-03-04 16:23:49 -0600

    changes

--- lib/ingredients.dart
+++ lib/ingredients.dart
@@ -11,29 +11,63 @@ class ingredientDropDown extends StatefulWidget {
 
 class _ingredientDropDownState extends State<ingredientDropDown> {
   var selected = "select ingredients";
+  var myIngredients = <String>[];
+
+  Scaffold _displayMyIngredients() {
+    final tiles = myIngredients.map(
+      (ingredient) {
+        return ListTile(
+          title: Text(
+            ingredient,
+          ),
+        );
+      },
+    );
+
+    final divided = tiles.isNotEmpty
+        ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList()
+        : <Widget>[];
+
+    return Scaffold(
+      body: ListView(children: divided),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: selected,
-      icon: const Icon(Icons.arrow_downward),
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            DropdownButton<String>(
+              value: selected,
+              icon: const Icon(Icons.arrow_downward),
+              style: const TextStyle(color: Colors.deepPurple),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  selected = newValue!;
+                  myIngredients.add(newValue);
+                });
+              },
+              items: <String>['select ingredients', 'a', 'b', 'c', 'd']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            _displayMyIngredients(),
+          ],
+        ),
       ),
-      onChanged: (String? newValue) {
-        setState(() {
-          selected = newValue!;
-        });
-      },
-      items: <String>['select ingredients','a', 'b', 'c', 'd']
-        .map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-      }).toList(),
     );
   }
 }
