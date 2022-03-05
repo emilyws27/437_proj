6fd864fd429a3a2575d3706b27b3536976bf35c4
emilyws27 <69002400+emilyws27@users.noreply.github.com>
2022-03-05 15:34:24 -0600

    sign in working on android

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,18 @@ import 'package:flutter/services.dart';
 import 'package:zesty/bottom_nav_bar.dart';
 import 'package:zesty/ingredients.dart';
 import 'package:zesty/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(const MyApp());
+
+  runApp(const MyApp2());
 }
 
 // #docregion MyApp
@@ -36,6 +40,27 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const BottomNav(),
+    );
+  }
+// #enddocregion build
+}
+
+class MyApp2 extends StatelessWidget {
+  static final String title = 'Simulator';
+  const MyApp2({Key? key}) : super(key: key);
+
+  // #docregion build
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Zesty',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(0xFF, 0xFE, 0xD9, 0xD9),
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: const Login(),
     );
   }
 // #enddocregion build
---
+++ lib/user.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:zesty/main.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:zesty/main.dart';
+
+final GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      'email'
+    ]
+);
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+class _HomeState extends State<Login> {
+
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState() {
+    _googleSignIn.onCurrentUserChanged.listen((account) {
+      setState(() {
+        _currentUser = account;
+      });
+    });
+    _googleSignIn.signInSilently();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Google Sign in'),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: _buildWidget(),
+      ),
+    );
+  }
+
+  Widget _buildWidget(){
+    GoogleSignInAccount? user = _currentUser;
+    if(user != null){
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(2, 12, 2, 12),
+        child: Column(
+          children: [
+            ListTile(
+              leading: GoogleUserCircleAvatar(identity: user),
+              title:  Text(user.displayName ?? '', style: TextStyle(fontSize: 22),),
+              subtitle: Text(user.email, style: TextStyle(fontSize: 22)),
+            ),
+            const SizedBox(height: 20,),
+            const Text(
+              'Signed in successfully',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 10,),
+            ElevatedButton(
+                onPressed: signOut,
+                child: const Text('Sign out')
+            )
+          ],
+        ),
+      );
+    }else{
+      return Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 20,),
+            const Text(
+              'You are not signed in',
+              style: TextStyle(fontSize: 30),
+            ),
+            const SizedBox(height: 10,),
+            ElevatedButton(
+                onPressed: signIn,
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Sign in', style: TextStyle(fontSize: 30)),
+                )
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  void signOut(){
+    _googleSignIn.disconnect();
+  }
+
+  Future<void> signIn() async {
+    try{
+      print("Hello");
+      await _googleSignIn.signIn();
+    }catch (e){
+      print('Error signing in $e');
+    }
+  }
+
+}
+// class Login extends StatelessWidget {
+//   const Login({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Zesty'),
+//         ),
+//         body: Center(
+//             child: ElevatedButton(
+//                 child: const Text("go to home"),
+//                 onPressed: () {
+//                   print("HERE!");
+//                   Navigator.push(
+//                     context,
+//                     MaterialPageRoute(builder: (context) => const MyApp()),
+//                   );
+//                 })));
+//   }
+// }
+
