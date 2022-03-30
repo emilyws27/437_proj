a257720a5f7f2020ddc93aecf7d0a874ab9a3864
adamkw66 <a.knox-warshaw@wustl.edu>
2022-03-29 20:25:12 -0500

    making changes to recipe view

--- lib/recipe_details.dart
+++ lib/recipe_details.dart
@@ -5,25 +5,71 @@ import 'package:flutter/widgets.dart';
 class viewRecipe extends StatelessWidget {
   final DocumentSnapshot snapshot;
   const viewRecipe({Key? key, required this.snapshot}) : super(key: key);
-  final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Zesty',
-            style: TextStyle(
-                fontFamily: 'Cookie', fontSize: 35, color: Colors.black)),
-        centerTitle: true,
-        backgroundColor: Colors.amber[900],
-      ),
-      body: Column(
-        children: <Widget>[
-          Text(snapshot['title'], style: _biggerFont, textAlign: TextAlign.center,),
-          const Text('Ingredients', style: TextStyle(fontSize: 16.0), textAlign: TextAlign.left),
-          Text(snapshot['ingredients'], style: TextStyle(fontSize: 14.0),textAlign: TextAlign.left )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Zesty',
+              style: TextStyle(
+                  fontFamily: 'Cookie', fontSize: 35, color: Colors.black)),
+          centerTitle: true,
+          backgroundColor: Colors.amber[900],
+        ),
+        body: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                ),
+                child: Text(
+                  snapshot['title'].toLowerCase(),
+                  style: const TextStyle(
+                      fontSize: 28.0, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 5,
+                    ),
+                    child: Text('Ingredients',
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            decoration: TextDecoration.underline)),
+                  )),
+              ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: snapshot['ingredients'].length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 5,
+                        ),
+                        child: Text(
+                            snapshot['ingredients'][index]['quantity'] +
+                                " " +
+                                snapshot['ingredients'][index]['ingredient'],
+                            style: const TextStyle(fontSize: 20.0)));
+                  }),
+              const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 5,
+                    ),
+                    child: Text('Directions',
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            decoration: TextDecoration.underline)),
+                  )),
+            ],
+          ),
+        );
   }
 }
